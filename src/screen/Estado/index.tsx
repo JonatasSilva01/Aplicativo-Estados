53e66ade375cd0a1525787aa1a0522480bcec32f
JonatasSilva01 <jhownsantossilva@gmail.com>
2022-05-24 18:12:19 -0300

    enviando alterações 2

--- src/screen/Estado/index.tsx
+++ src/screen/Estado/index.tsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
-import Botao from "../../components/Botao";
+import { View, FlatList, StyleSheet, Text } from "react-native";
 import { api } from "../../service/api";
-
-// pagina principal
-// ?orderBy=nome
 
 interface ApiEstado{
     id: number;
@@ -13,12 +9,56 @@ interface ApiEstado{
     regiao: string;
 }
 
+const style = StyleSheet.create({
+    container:{
+        flex: 1,
+
+        backgroundColor: '#eeeeee',
+        padding: 20,
+
+        justifyContent: 'center',
+    },
+    btnItem: {
+        paddingVertical: 10,
+        marginTop: 5,
+
+        backgroundColor: '#FFFFFF',
+        flexDirection: 'row',
+
+        borderRadius: 10,
+    },
+    avatarSigla: {
+        width: 60,
+        height: 60,
+
+        backgroundColor: '#E76F51',
+        borderRadius: 40,
+
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 5,
+    },
+    avatarTxt: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+    },
+    legenda: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#000',
+
+        marginLeft: 10,
+        alignSelf: 'center',
+    },
+})
+
 export default function Estado(){
     const[estados, setEstados] = useState<ApiEstado[]>([])
 
     async function getEstados(){
         const resposta = await api.get('?orderBy=nome')
-        console.log(resposta.data)
+        setEstados(resposta.data)
     }
 
     useEffect(() => {
@@ -26,8 +66,25 @@ export default function Estado(){
     },[])
 
     return(
-        <View>
-            <Botao legenda="Botao 2" />
+        <View style={style.container}>
+            <FlatList<ApiEstado>
+                style={{flex: 1}}
+                data={estados}
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(estados)=> String(estados.id)}
+                renderItem={({item})=>{
+                    return(
+                        <View style={style.btnItem}>
+                            <View style={style.avatarSigla}>
+                                <Text style={style.avatarTxt}>
+                                    {item.sigla}
+                                </Text>
+                            </View>
+                            <Text style={style.legenda}>{item.nome}</Text>
+                        </View>
+                    )
+                }}
+            />
         </View>
     )
 }
--- src/service/api.tsx
+++ src/service/api.tsx
@@ -2,3 +2,4 @@ import axios from "axios";
 export const api = axios.create({
     baseURL:'https://servicodados.ibge.gov.br/api/v1/localidades/estados',
 })
+// essa pasta esta pegando os dados de uma api externa.
